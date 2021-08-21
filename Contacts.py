176045d23211d3d2ca5d992e39e5b9ed37e71315
IvanGrande <Ivan.enriquez.grande@gmail.com>
2021-08-21 13:32:43 -0500

    Imagen added en la interfaz

--- Contacts.py
+++ Contacts.py
@@ -2,10 +2,20 @@ from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry
 from tkinter import ttk
 
 class Contacts:
-    def __init__(self):
+    def __init__(self, root):
         self.root = root
+        self.create_left_icon()
+
+    def create_left_icon(self):
+        photo = PhotoImage(file = 'Resources/Logo.gif')
+        label = Label(image = photo)
+        label.image = photo
+        label.grid(row = 0, column = 0)
+
+
 
 if __name__ == "__main__":
     root = Tk()
     root.title('Contacts list')
+    app = Contacts(root)
     root.mainloop()
