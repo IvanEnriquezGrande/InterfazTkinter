9dda790aad95170fa069117dba68729eceda4202
IvanGrande <Ivan.enriquez.grande@gmail.com>
2021-08-21 14:14:09 -0500

    Tree view added

--- Contacts.py
+++ Contacts.py
@@ -6,6 +6,8 @@ class Contacts:
         self.root = root
         self.create_left_icon()
         self.create_label_frame()
+        self.create_message_area()
+        self.create_tree_view()
 
     def create_left_icon(self):
         photo = PhotoImage(file = 'Resources/Logo.gif')
@@ -26,6 +28,17 @@ class Contacts:
         self.numfield = Entry(labelframe)
         self.numfield.grid(row = 3, column = 2, sticky = W, padx = 5, pady = 2)
         Button(labelframe, text = 'Add contact', command="", bg = "blue", fg = "white").grid(row = 4, column = 2, sticky = E, padx = 5, pady = 5)
+
+    def create_message_area(self):
+        self.message = Label(text = "", fg = "red")
+        self.message.grid(row = 3, column = 1, sticky = W)
+
+    def create_tree_view(self):
+        self.tree = ttk.Treeview(height = 10, columns = ('email', 'number'))
+        self.tree.grid(row = 6, column = 0, columnspan = 3)
+        self.tree.heading('#0', text = 'Name', anchor = W)
+        self.tree.heading('email', text = 'Email adress', anchor = W)
+        self.tree.heading('number', text = 'Phone number', anchor = W)
 
 if __name__ == "__main__":
     root = Tk()
