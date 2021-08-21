d6ad05c9c125f9f8d8376c5338be02452fef1a5e
IvanGrande <Ivan.enriquez.grande@gmail.com>
2021-08-21 13:21:39 -0500

    Interfaz.py added

---
+++ Contacts.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry, END, StringVar, Scrollbar, Toplevel
+from tkinter import ttk
+
+class Contacts:
+    def __init__(self):
+        self.root = root
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title('Contacts list')
+    root.mainloop()
